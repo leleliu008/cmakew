4e1a55d3e9040a06f8000107e42b0d4584ba53e4
leleliu008 <leleliu008@gmail.com>
2021-05-05 22:07:24 +0800

    optimize

--- build.bat
+++ build.bat
@@ -1,3 +1,7 @@
+if ""%1""=="""" (set VS_VERSION=2019) else (set VS_VERSION=%1)
+
+"C:\Program Files (x86)\Microsoft Visual Studio\%VS_VERSION%\BuildTools\Common7\Tools\VsDevCmd.bat"
+
 cmake ^
     -DCMAKE_EXPORT_COMPILE_COMMANDS=ON ^
     -DCMAKE_VERBOSE_MAKEFILE=ON ^
